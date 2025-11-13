5ca88428fc98481821d734cad265cad366432753
kegra <nukegarara@yahoo.co.jp>
2025-11-14 00:51:53 +0900

    removed unused def

--- src/core/renderer/materialrender.hpp
+++ src/core/renderer/materialrender.hpp
@@ -1,18 +1,13 @@
 #pragma once
 
 #include "../container.hpp"
-#include <map>
-#include <unordered_map>
 #include <vulkan/vulkan.hpp>
 
 namespace Pelican {
 
-struct Material {
-    uint32_t pipeline_id;
-};
-
 class MaterialRenderer {
     DependencyContainer &con;
+
   public:
     MaterialRenderer(DependencyContainer &con);
     void render(vk::CommandBuffer cmd_buf) const;
