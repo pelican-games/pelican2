552cf91f54c046a76aaca094b7ea890cf3412eba
kegra <nukegarara@yahoo.co.jp>
2025-11-18 17:31:43 +0900

    FastModuleContainer

--- src/core/container.hpp
+++ src/core/container.hpp
@@ -2,10 +2,48 @@
 
 #include "./log.hpp"
 #include <memory>
+#include <optional>
 #include <typeindex>
 #include <unordered_map>
 
+#define DECLARE_MODULE(name) class name : public ModuleBase<name>
+#define GET_MODULE(name) FastModuleContainer::get<name>()
+
 namespace Pelican {
+
+template <class T> struct ModuleBase {
+    static std::optional<T> &__get() {
+        static std::optional<T> obj;
+        return obj;
+    }
+};
+
+class FastModuleContainer {
+    using CleanerFunctionType = void();
+    static inline std::vector<std::pair<std::type_index, CleanerFunctionType *>> cleaners;
+
+  public:
+    template <class T> static T &get() {
+        std::optional<T> &obj_ref = T::__get();
+        if (!obj_ref.has_value()) {
+            const auto name = typeid(T).name();
+            LOG_INFO(logger, "Module [{}] initializing...", name);
+            obj_ref.emplace();
+            cleaners.push_back({std::type_index{typeid(T)}, []() { T::__get().reset(); }});
+            LOG_INFO(logger, "Module [{}] initialized", name);
+        }
+        return obj_ref.value();
+    }
+    ~FastModuleContainer() {
+        while (!cleaners.empty()) {
+            const auto [id, cleaner] = cleaners.back();
+            LOG_INFO(logger, "Module [{}] destroying...", id.name());
+            cleaner();
+            cleaners.pop_back();
+            LOG_INFO(logger, "Module [{}] destroyed", id.name());
+        }
+    }
+};
 
 class DependencyContainer {
     class IStore {
