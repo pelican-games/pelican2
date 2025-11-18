dfae6a4e8193fcfb06e16f6160bdfe0a59ffadbf
kegra <nukegarara@yahoo.co.jp>
2025-11-18 22:53:44 +0900

    ECS System registration

--- src/core/ecs/core.hpp
+++ src/core/ecs/core.hpp
@@ -1,14 +1,18 @@
 #pragma once
 
+#include "../container.hpp"
 #include <array>
 #include <memory>
+#include <queue>
 #include <typeindex>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 namespace Pelican {
 
 using EntityId = uint64_t;
+using SystemId = uint64_t;
 
 DECLARE_MODULE(ECSCore) {
     using ChunkIndex = uint32_t;
@@ -97,7 +101,7 @@ DECLARE_MODULE(ECSCore) {
     ECSCore() {}
     ~ECSCore() {}
 
-    template <class... TComponent> EntityId insert(TComponent &&...component) {
+    template <class... TComponent> EntityId insert(TComponent && ...component) {
         // find suitable chunk
         ChunkIndex chunk_index = UINT32_MAX;
         for (uint32_t i = 0; i < chunks_storage.size(); i++) {
@@ -132,59 +136,81 @@ DECLARE_MODULE(ECSCore) {
 
     void compaction() { /* TODO */ }
 
-    template <class... TComponent> struct QueryResult {
-        std::vector<ECSComponentChunk *> chunks;
-
-        struct Iterator {
-            ECSComponentChunk **pp_chunks;
-            size_t count_remains, chunk_remains;
-            std::tuple<TComponent *...> ptrs;
-            std::tuple<TComponent &...> operator*() { return std::tie(*std::get<TComponent *>(ptrs)...); }
-            Iterator &operator++() {
-                count_remains--;
-                if (count_remains > 0) {
-                    ((std::get<TComponent *>(ptrs)++), ...);
-                    return *this;
-                }
-
-                // reached chunk bound
-                chunk_remains--;
-                if (chunk_remains > 0) {
-                    pp_chunks++;
-                    const auto p_chunk = *pp_chunks;
-                    auto &chunk = *p_chunk;
-                    count_remains = chunk.count;
-                    ptrs = std::make_tuple(chunk.get<TComponent>()...);
-                    return *this;
-                }
-                return *this;
-            }
-            bool operator!=(const Iterator &v) { return chunk_remains != v.chunk_remains; }
-        };
-        Iterator begin() {
-            if (chunks.size() == 0)
-                return end();
-            auto &first_chunk = *chunks[0];
-            return Iterator{
-                .pp_chunks = chunks.data(),
-                .count_remains = first_chunk.count,
-                .chunk_remains = chunks.size(),
-                .ptrs = std::make_tuple(first_chunk.get<TComponent>()...),
-            };
-        }
-        Iterator end() {
-            return Iterator{
-                .chunk_remains = 0,
-            };
-        }
+  private:
+    struct InternalSystemWrapper {
+        std::vector<SystemId> depends_list;
+        std::unordered_set<SystemId> depended_by;
+        void *system_ref;
+        void (*p_func)(ECSCore &ecs, void *system);
     };
-    template <class... TComponent> QueryResult<TComponent...> query() {
-        QueryResult<TComponent...> result;
-        for (auto &chunk : chunks_storage) {
-            if (chunk.has_all<TComponent...>() && chunk.count > 0)
-                result.chunks.push_back(&chunk);
+    uint64_t systems_counter = 0;
+    std::unordered_map<SystemId, InternalSystemWrapper> systems;
+
+  public:
+    template <class TSystem, class... TComponents>
+    SystemId registerSystem(TSystem & system, std::vector<SystemId> && depends_list) {
+        static_assert(std::is_same<typename TSystem::QueryComponents, std::tuple<TComponents *...>>::value);
+        SystemId new_sys_id = systems_counter++;
+
+        for (const auto depends : depends_list) {
+            systems.at(depends).depended_by.insert(new_sys_id);
         }
-        return result;
+        systems.insert({
+            new_sys_id,
+            InternalSystemWrapper{
+                .depends_list = std::move(depends_list),
+                .depended_by = {},
+                .system_ref = &system,
+                .p_func =
+                    [](ECSCore &ecs, void *p_system) {
+                        TSystem &system = *static_cast<TSystem *>(p_system);
+                        for (auto &chunk : ecs.chunks_storage) {
+                            if (chunk.has_all<TComponents...>())
+                                system.process(std::make_tuple(chunk.get<TComponents>()...), chunk.count);
+                        }
+                    },
+            },
+        });
+
+        return new_sys_id;
+    }
+    void unregisterSystem(SystemId system_id) {
+        for (const auto depends : systems.at(system_id).depends_list) {
+            systems.at(depends).depended_by.erase(system_id);
+        }
+        systems.erase(system_id);
+    }
+
+    void update() {
+        std::vector<SystemId> sorted_systems;
+
+        // topological sort
+        std::queue<SystemId> que;
+        std::unordered_map<SystemId, size_t> dependency_graph;
+
+        for (const auto &[id, sys] : systems) {
+            dependency_graph.insert({id, sys.depends_list.size()});
+            if (sys.depends_list.empty())
+                que.push(id);
+        }
+        while (!que.empty()) {
+            auto next = que.front();
+            que.pop();
+            sorted_systems.push_back(next);
+
+            for (const auto depended : systems.at(next).depended_by) {
+                auto &dep_count = dependency_graph.at(depended);
+                dep_count--;
+                if (dep_count == 0)
+                    que.push(depended);
+            }
+        }
+
+        // invoke
+        for (auto sys_id : sorted_systems) {
+            auto &sys = systems.at(sys_id);
+            sys.p_func(*this, sys.system_ref);
+        }
     }
 };
 
