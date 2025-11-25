79c12e30c9fe00661228cd7ee0561eb5e9852ec1
kegra <nukegarara@yahoo.co.jp>
2025-11-25 09:12:30 +0900

    separate predefined component declaration

--- src/core/ecs/componentinfo.hpp
+++ src/core/ecs/componentinfo.hpp
@@ -7,11 +7,7 @@
 #include <unordered_map>
 
 #include "component.hpp"
-
 #include "entity.hpp"
-#include "predefined/camera.hpp"
-#include "predefined/modelview.hpp"
-#include "predefined/transform.hpp"
 
 namespace Pelican {
 
@@ -48,10 +44,6 @@ template <class T> struct ComponentIdByType {
         static constexpr ComponentId value = _id;                                                                      \
     };
 
-// predefined
 DECLARE_COMPONENT(EntityId, 0);
-DECLARE_COMPONENT(TransformComponent, 1);
-DECLARE_COMPONENT(SimpleModelViewComponent, 2);
-DECLARE_COMPONENT(CameraComponent, 3);
 
 } // namespace Pelican
--- src/core/ecs/predefined.hpp
+++ src/core/ecs/predefined.hpp
@@ -2,11 +2,21 @@
 
 #include "../container.hpp"
 
+#include "componentinfo.hpp"
+
+#include "predefined/camera.hpp"
+#include "predefined/modelview.hpp"
+#include "predefined/transform.hpp"
+
 namespace Pelican {
 
 DECLARE_MODULE(ECSPredefinedRegistration) {
   public:
     void reg();
 };
+
+DECLARE_COMPONENT(TransformComponent, 1);
+DECLARE_COMPONENT(SimpleModelViewComponent, 2);
+DECLARE_COMPONENT(CameraComponent, 3);
 
 } // namespace Pelican
