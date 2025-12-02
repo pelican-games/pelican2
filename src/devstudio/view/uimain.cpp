98c56479e43c2f36648a5f4dde62221eb3aa1525
kegra <nukegarara@yahoo.co.jp>
2025-12-02 16:42:44 +0900

    error exiting

--- src/devstudio/view/uimain.cpp
+++ src/devstudio/view/uimain.cpp
@@ -12,6 +12,14 @@ int uimain(int argc, char **argv) {
 
     std::cout << "starting Pelican Studio..." << std::endl;
 
+    // necessary for exiting if error
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() { 
+            std::cerr << "failed to launch Qt application" << std::endl;
+            QCoreApplication::exit(-1); 
+        },
+        Qt::QueuedConnection);
+
     engine.load(QUrl(QStringLiteral("qrc:/qt/qml/pelican_studio/MainWindow.qml")));
     return app.exec();
 }
