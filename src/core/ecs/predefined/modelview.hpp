d95af1fa1566cdad3a6cef973208784d6ff13b55
kegra <nukegarara@yahoo.co.jp>
2025-12-17 21:19:31 +0900

    serializer impl for predefined components

--- src/core/ecs/predefined/modelview.hpp
+++ src/core/ecs/predefined/modelview.hpp
@@ -7,6 +7,9 @@ namespace Pelican {
 
 struct SimpleModelViewComponent {
     ModelInstanceId model_instance_id;
+    std::string model_name;
+
+    template <class T> void ref(T &ar) { ar.prop("model", model_name); }
 };
 
 } // namespace Pelican
