6b154c434e7f4b5a8e376e39b6d582d7ae17aad5
enjoyoriori <enjoyorioriavi@gmail.com>
2026-05-31 21:53:55 +0900

    Extract render pass frame setup

--- src/core/vkcore/render_pass_executor.cpp
+++ src/core/vkcore/render_pass_executor.cpp
@@ -4,90 +4,11 @@
 #include "../renderer/fullscreenpassrenderer.hpp"
 #include "../renderer/materialrender.hpp"
 #include "../renderer/uirenderer.hpp"
-#include "util.hpp"
-#include <vector>
+#include "render_pass_frame_setup.hpp"
 
 namespace Pelican {
 
 namespace {
-
-vk::Extent2D getTargetExtent(const FrameRenderContext &frame, const PassDefinition &pass_def,
-                             RenderTargetContainer &rt_container) {
-    for (const auto &rt_id : pass_def.output_color) {
-        if (isConcreteRenderTarget(rt_id)) {
-            const auto &output_rt = rt_container.get(rt_id);
-            return vk::Extent2D{output_rt.image.extent.width, output_rt.image.extent.height};
-        }
-    }
-    if (isConcreteRenderTarget(pass_def.output_depth)) {
-        const auto &output_rt = rt_container.get(pass_def.output_depth);
-        return vk::Extent2D{output_rt.image.extent.width, output_rt.image.extent.height};
-    }
-    return frame.extent;
-}
-
-void transitionOutputsToAttachmentLayouts(vk::CommandBuffer cmd_buf, const PassDefinition &pass_def,
-                                          RenderTargetContainer &rt_container, VulkanUtils &vk_utils,
-                                          RenderTargetLayoutTracker &layout_tracker) {
-    for (const auto &rt_id : pass_def.output_color) {
-        layout_tracker.transition(cmd_buf, rt_container, vk_utils, rt_id, vk::ImageLayout::eColorAttachmentOptimal);
-    }
-    layout_tracker.transition(cmd_buf, rt_container, vk_utils, pass_def.output_depth,
-                              vk::ImageLayout::eDepthAttachmentOptimal);
-}
-
-void transitionInputsToShaderRead(vk::CommandBuffer cmd_buf, const PassDefinition &pass_def,
-                                  RenderTargetContainer &rt_container, VulkanUtils &vk_utils,
-                                  RenderTargetLayoutTracker &layout_tracker) {
-    for (const auto &rt_id : pass_def.input_targets) {
-        layout_tracker.transition(cmd_buf, rt_container, vk_utils, rt_id, vk::ImageLayout::eShaderReadOnlyOptimal);
-    }
-}
-
-std::vector<vk::RenderingAttachmentInfo> createColorAttachments(const FrameRenderContext &frame,
-                                                                const PassDefinition &pass_def,
-                                                                RenderTargetContainer &rt_container) {
-    std::vector<vk::RenderingAttachmentInfo> color_attachments;
-    color_attachments.reserve(pass_def.output_color.size());
-
-    for (const auto &rt_id : pass_def.output_color) {
-        vk::RenderingAttachmentInfo color_att;
-        if (isSwapchainRenderTarget(rt_id)) {
-            color_att.imageView = frame.color_attachment;
-        } else {
-            color_att.imageView = rt_container.get(rt_id).image_view.get();
-        }
-
-        color_att.imageLayout = vk::ImageLayout::eColorAttachmentOptimal;
-        color_att.loadOp = pass_def.color_load_op;
-        color_att.storeOp = pass_def.color_store_op;
-        color_att.clearValue.color = pass_def.clear_color;
-        color_attachments.push_back(color_att);
-    }
-
-    return color_attachments;
-}
-
-vk::RenderingAttachmentInfo createDepthAttachment(const PassDefinition &pass_def,
-                                                  RenderTargetContainer &rt_container) {
-    vk::RenderingAttachmentInfo depth_attachment;
-    const auto &depth_rt = rt_container.get(pass_def.output_depth);
-    depth_attachment.imageView = depth_rt.image_view.get();
-    depth_attachment.imageLayout = vk::ImageLayout::eDepthAttachmentOptimal;
-    depth_attachment.loadOp = vk::AttachmentLoadOp::eClear;
-    depth_attachment.storeOp = vk::AttachmentStoreOp::eDontCare;
-    depth_attachment.clearValue.depthStencil = vk::ClearDepthStencilValue{1.0f, 0};
-    return depth_attachment;
-}
-
-void setDynamicViewportAndScissor(vk::CommandBuffer cmd_buf, vk::Extent2D extent) {
-    vk::Viewport viewport{0.0f, 0.0f, static_cast<float>(extent.width), static_cast<float>(extent.height), 0.0f,
-                          1.0f};
-    cmd_buf.setViewport(0, viewport);
-
-    vk::Rect2D scissor{{0, 0}, extent};
-    cmd_buf.setScissor(0, scissor);
-}
 
 void renderMaterialPass(vk::CommandBuffer cmd_buf, PassId pass_id, const PassDefinition &pass_def) {
     auto &mat_renderer = GET_MODULE(MaterialRenderer);
@@ -129,10 +50,10 @@ void RenderPassExecutor::execute(const FrameRenderContext &frame, const PassDefi
     auto &rt_container = GET_MODULE(RenderTargetContainer);
     auto &vk_utils = GET_MODULE(VulkanUtils);
     const auto cmd_buf = frame.cmd_buf;
-    const auto target_extent = getTargetExtent(frame, pass_def, rt_container);
+    const auto target_extent = getRenderPassTargetExtent(frame, pass_def, rt_container);
 
-    transitionInputsToShaderRead(cmd_buf, pass_def, rt_container, vk_utils, layout_tracker);
-    transitionOutputsToAttachmentLayouts(cmd_buf, pass_def, rt_container, vk_utils, layout_tracker);
+    transitionPassInputsToShaderRead(cmd_buf, pass_def, rt_container, vk_utils, layout_tracker);
+    transitionPassOutputsToAttachmentLayouts(cmd_buf, pass_def, rt_container, vk_utils, layout_tracker);
 
     if (pass_def.isUi()) {
         if (pass_def.output_color.empty()) {
---
+++ src/core/vkcore/render_pass_frame_setup.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../renderingpass/renderingpasscontainer.hpp"
+#include "../renderingpass/rendertargetcontainer.hpp"
+#include "render_target_layout_tracker.hpp"
+#include "rendertarget.hpp"
+#include "util.hpp"
+#include <vector>
+#include <vulkan/vulkan.hpp>
+
+namespace Pelican {
+
+vk::Extent2D getRenderPassTargetExtent(const FrameRenderContext &frame, const PassDefinition &pass_def,
+                                       RenderTargetContainer &rt_container);
+void transitionPassOutputsToAttachmentLayouts(vk::CommandBuffer cmd_buf, const PassDefinition &pass_def,
+                                              RenderTargetContainer &rt_container, VulkanUtils &vk_utils,
+                                              RenderTargetLayoutTracker &layout_tracker);
+void transitionPassInputsToShaderRead(vk::CommandBuffer cmd_buf, const PassDefinition &pass_def,
+                                      RenderTargetContainer &rt_container, VulkanUtils &vk_utils,
+                                      RenderTargetLayoutTracker &layout_tracker);
+std::vector<vk::RenderingAttachmentInfo> createColorAttachments(const FrameRenderContext &frame,
+                                                                const PassDefinition &pass_def,
+                                                                RenderTargetContainer &rt_container);
+vk::RenderingAttachmentInfo createDepthAttachment(const PassDefinition &pass_def,
+                                                  RenderTargetContainer &rt_container);
+void setDynamicViewportAndScissor(vk::CommandBuffer cmd_buf, vk::Extent2D extent);
+
+} // namespace Pelican
