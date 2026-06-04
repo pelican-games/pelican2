179ab4e47c3810aa5023b8ead7e017a394184cc8
enjoyoriori <enjoyorioriavi@gmail.com>
2026-06-04 23:37:18 +0900

    Clarify fullscreen runtime dependencies

--- src/core/renderingpass/renderingpassruntimecompiler.cpp
+++ src/core/renderingpass/renderingpassruntimecompiler.cpp
@@ -12,6 +12,13 @@ namespace Pelican {
 
 namespace {
 
+struct FullscreenRuntimeDependencies {
+    RenderTarget &render_target;
+    RenderTargetContainer &render_target_container;
+    ShaderContainer &shader_container;
+    FullscreenPassContainer &fullscreen_pass_container;
+};
+
 vk::Format resolveFirstColorFormat(const PassDefinition &pass_def, RenderTarget &rt_module,
                                    RenderTargetContainer &rt_container) {
     if (pass_def.output_color.empty()) {
@@ -32,7 +39,9 @@ PassId passIndexToPassId(size_t pass_index) {
     return PassId{static_cast<int>(pass_index)};
 }
 
-void requireFullscreenDependencies(const PassDefinition &pass_def, const RenderingPassRuntimeDependencies &dependencies) {
+FullscreenRuntimeDependencies requireFullscreenDependencies(
+    const PassDefinition &pass_def,
+    const RenderingPassRuntimeDependencies &dependencies) {
     if (dependencies.render_target == nullptr || dependencies.render_target_container == nullptr ||
         dependencies.shader_container == nullptr || dependencies.fullscreen_pass_container == nullptr) {
         throw std::runtime_error(
@@ -40,33 +49,37 @@ void requireFullscreenDependencies(const PassDefinition &pass_def, const Renderi
             "and fullscreen pass container dependencies: " +
             pass_def.name);
     }
+    return FullscreenRuntimeDependencies{
+        *dependencies.render_target,
+        *dependencies.render_target_container,
+        *dependencies.shader_container,
+        *dependencies.fullscreen_pass_container,
+    };
 }
 
-PassId compileFullscreenPass(const PassDefinition &pass_def, const RenderingPassRuntimeDependencies &dependencies) {
-    requireFullscreenDependencies(pass_def, dependencies);
-
-    auto &rt_module = *dependencies.render_target;
-    auto &rt_container = *dependencies.render_target_container;
-    auto &shader_container = *dependencies.shader_container;
-    auto &fs_container = *dependencies.fullscreen_pass_container;
-
-    const auto color_format = resolveFirstColorFormat(pass_def, rt_module, rt_container);
+PassId compileFullscreenPass(const PassDefinition &pass_def, FullscreenRuntimeDependencies dependencies) {
+    const auto color_format =
+        resolveFirstColorFormat(pass_def, dependencies.render_target, dependencies.render_target_container);
     const auto &fullscreen_info = pass_def.fullscreenInfo();
     const auto vert_shader_data = readBinaryFile(fullscreen_info.vert_shader_path);
     const auto frag_shader_data = readBinaryFile(fullscreen_info.frag_shader_path);
-    const auto vert_shader_id = shader_container.registerShader(vert_shader_data.size(), vert_shader_data.data());
-    const auto frag_shader_id = shader_container.registerShader(frag_shader_data.size(), frag_shader_data.data());
-    const auto vert_shader = shader_container.getShader(vert_shader_id);
-    const auto frag_shader = shader_container.getShader(frag_shader_id);
+    const auto vert_shader_id =
+        dependencies.shader_container.registerShader(vert_shader_data.size(), vert_shader_data.data());
+    const auto frag_shader_id =
+        dependencies.shader_container.registerShader(frag_shader_data.size(), frag_shader_data.data());
+    const auto vert_shader = dependencies.shader_container.getShader(vert_shader_id);
+    const auto frag_shader = dependencies.shader_container.getShader(frag_shader_id);
 
-    const auto pipeline_id = fs_container.registerFullscreenPass(color_format, vert_shader, frag_shader);
+    const auto pipeline_id =
+        dependencies.fullscreen_pass_container.registerFullscreenPass(color_format, vert_shader, frag_shader);
     if (pipeline_id.value > static_cast<uint32_t>(std::numeric_limits<int>::max())) {
         throw std::runtime_error("Fullscreen pipeline id is too large");
     }
     const PassId pass_id{static_cast<int>(pipeline_id.value)};
 
     if (!pass_def.input_targets.empty()) {
-        fs_container.setInputTextures(pass_id, pass_def.input_targets, rt_container);
+        dependencies.fullscreen_pass_container.setInputTextures(
+            pass_id, pass_def.input_targets, dependencies.render_target_container);
     }
 
     return pass_id;
@@ -85,7 +98,9 @@ CompiledRenderingPass compileRenderingPassRuntime(const RenderingPassDefinition 
     for (size_t i = 0; i < definition.passes.size(); ++i) {
         const auto &pass_def = definition.passes[i];
         if (pass_def.isFullscreen()) {
-            compiled_pass.passes.push_back(CompiledPass{pass_def, compileFullscreenPass(pass_def, dependencies)});
+            const auto fullscreen_dependencies = requireFullscreenDependencies(pass_def, dependencies);
+            compiled_pass.passes.push_back(
+                CompiledPass{pass_def, compileFullscreenPass(pass_def, fullscreen_dependencies)});
         } else {
             compiled_pass.passes.push_back(CompiledPass{pass_def, passIndexToPassId(i)});
         }
