bb12936584c0e6c277174dbc77af67b5ccdcb50c
enjoyoriori <enjoyorioriavi@gmail.com>
2026-07-08 14:00:26 +0900

    Implement pelican_cli project init

--- src/devcli/main.cpp
+++ src/devcli/main.cpp
@@ -1,5 +1,6 @@
 #include "distconfig.hpp"
 #include "importcommand.hpp"
+#include "projectinit.hpp"
 
 #include "battery/embed.hpp"
 #include <argparse/argparse.hpp>
@@ -26,6 +27,9 @@ int main(int argc, char *argv[]) {
     }
     if (argc > 1 && std::string_view{argv[1]} == "dist-config") {
         return Pelican::DevCli::runDistConfigCommand(argc - 1, argv + 1);
+    }
+    if (argc > 1 && std::string_view{argv[1]} == "project") {
+        return Pelican::DevCli::runProjectCommand(argc - 1, argv + 1);
     }
 
     argparse::ArgumentParser program("Pelican Cli");
